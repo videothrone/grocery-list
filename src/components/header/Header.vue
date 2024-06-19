ca4113f11036339dde6b6051c837961ecbe7eee7
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-06-19 13:18:18 +0200

    ✨ Add header icon

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -3,7 +3,8 @@
     <v-row>
       <v-col cols="12">
         <h1 class="header__headline display-1 text-center">
-          Grocery<span class="header__headline--orange"> List</span>
+          <v-icon class="header__headline-icon">mdi-cart-outline</v-icon>
+          <div>Grocery List</div>
         </h1>
       </v-col>
     </v-row>
